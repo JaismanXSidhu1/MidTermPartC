897c7e68e591d5d278a7f36f9cca12cae384ebc3
Jaisman Singh Sidhu <sidhu722@sheridancollege.ca>
2024-02-21 09:55:26 -0500

    added comments to jaisman.java file

---
+++ src/jaisman/jaisman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jaisman;
+
+/**
+ *
+ * @author jaismansinghsidhu
+ */
+public class jaisman {
+    /**my name is jaisman singh sidhu
+     * and this is my midterm partB
+     * /
+    
+}
